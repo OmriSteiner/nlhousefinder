622bbd9878e48ee8fe71d05e81cc4abaf95f69ae
Omri Steiner <omri@steiners.co.il>
2025-01-05 14:55:19 +0100

    add telegram bot stub

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,36 @@
 mod persistence;
 mod scraping;
 
-fn main() {
-    println!("Hello, world!");
+use teloxide::{prelude::*, utils::command::BotCommands};
+
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase")]
+enum Command {
+    #[command(description = "Intro message")]
+    Start,
+    #[command(description = "Subscribe to new properties")]
+    Subscribe,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let bot = Bot::from_env();
+
+    Command::repl(bot, handle_message).await;
+
+    Ok(())
+}
+
+async fn handle_message(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Start => {
+            bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                .await?
+        }
+        Command::Subscribe => bot.send_message(msg.chat.id, "Subscribed!").await?,
+    };
+
+    Ok(())
 }
